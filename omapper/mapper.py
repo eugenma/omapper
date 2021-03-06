ffc0889780f611e5830e89cd6d4dcd8eaac9c09e
EugenM <eugen.massini@gmail.com>
2021-03-06 21:30:34 +0100

    Improved mappers.

--- omapper/mapper.py
+++ omapper/mapper.py
@@ -1,6 +1,6 @@
 import collections
 import inspect
-from typing import Callable, Any, Sequence, OrderedDict, Optional, MutableMapping
+from typing import Callable, Any, Sequence, OrderedDict, Optional, MutableMapping, Dict
 
 try:
     from typing import get_origin, get_args
@@ -8,61 +8,66 @@ except ImportError:
     raise ImportError("Requires Python Version >= 3.8")
 
 
-MapEntry = Callable[[str, Any], Any]
-
-
-class Mappers:
-    @classmethod
-    def identity(cls, dest_attr: str, src_attr: Optional[str] = None) -> MapEntry:
-        return cls.transform(dest_attr, lambda e: e, src_attr)
-
-    @classmethod
-    def transform(cls, dest_attr: str, by: Callable[[Any, ], Any], src_attr: Optional[str] = None) -> MapEntry:
-        if not src_attr:
-            src_attr = dest_attr
-
-        def map_transform(_: str, src: Any) -> Any:
-            src_value = getattr(src, src_attr)
-            return by(src_value)
-
-        return map_transform
+MapEntry = Callable[[Any, ], Any]
+DefaultMapper = Callable[[str], Callable[[Any], Any]]
 
 
 class Mapper:
-    def __init__(self, src_type: type, dest_type: type, mappers: MutableMapping[str, MapEntry], explicit: bool = False):
-        self.mappers = mappers
+    def __init__(self, src_type: type, dest_type: type, mappers: Optional[MutableMapping[str, MapEntry]] = None,
+                 explicit: bool = False, default_mapper: Optional[DefaultMapper] = None):
+        self.mappers = {}  # type: Dict[str, MapEntry]
+        if mappers:
+            self.mappers.update(mappers)
 
-        dest_attrs = Mapper.get_attrs(dest_type)
+        self.dest_attrs = Mapper.get_all_attrs(dest_type)
+        if not self.dest_attrs:
+            raise ValueError(f"The type of 'dest_type' {dest_type} does not has any parameters in the constructor.")
+
         if explicit:
-            self.__check_explicit_only(dest_attrs, dest_type)
+            self.__check_explicit_only(self.dest_attrs, dest_type)
         else:
-            implicit_attrs = filter(lambda e: e not in mappers, dest_attrs)
-            for attr in implicit_attrs:
-                self.mappers[attr] = Mappers.identity(attr)
+            implicit_mapper = default_mapper if default_mapper else lambda name: lambda src: getattr(src, name)
+
+            implicit_attrs = filter(lambda e: e not in self.mappers, self.dest_attrs)
+            self.mappers.update((a, implicit_mapper(a)) for a in implicit_attrs)
+
+        missing = [attr for attr in self.mappers.keys() if attr not in self.dest_attrs]
+        if missing:
+            mapper_keys = ', '.join((f"'{k}'" for k in missing))
+            raise ValueError(
+                f"Configured attributes {mapper_keys} not found in target ctor '{dest_type}'. "
+                f"Available attributes {self.dest_attrs}.")
 
         self.dest_type = dest_type
         self.source_type = src_type
+
+    def __call__(self, src: Any) -> Any:
+        values = self._mapped_values(src)
+        try:
+            return self.dest_type(**values)
+        except Exception:
+            str_values = map(lambda e: f'"{e[0]}"="{repr(e[1])}"', values.items())
+            joined = ', '.join(str_values)
+            msg = f"Failed to create dest '{self.dest_type}' with params {joined}."
+            raise ValueError(msg)
+
+    def _mapped_values(self, src: Any) -> OrderedDict[str, Any]:
+        values = collections.OrderedDict()
+        for dest_attr, mapper in self.mappers.items():
+            dest_value = mapper(src)
+            values[dest_attr] = dest_value
+        return values
+
+    @staticmethod
+    def get_all_attrs(dest_type: type) -> Sequence[str]:
+        # MyPy throws an error on Ctor __init__, but there are no other means to access it.
+        dest_params = inspect.signature(dest_type.__init__).parameters  # type: ignore
+        all_attrs = map(lambda e: e[0], dest_params.items())
+        dest_attrs = [e for e in all_attrs if e != 'self']
+        return dest_attrs
 
     def __check_explicit_only(self, dest_attrs: Sequence[str], dest_type: type):
         missing_attrs = [e for e in dest_attrs if e not in self.mappers]
         missing_attrs_str = ', '.join(f"'{dest_type.__name__}.{e}'" for e in missing_attrs)
         if missing_attrs_str:
             raise ValueError(f"Explicit Mapper is missing maps to following target fields {missing_attrs_str}.")
-
-    def _mapped_values(self, src: Any) -> OrderedDict[str, Any]:
-        values = collections.OrderedDict()
-        for dest_attr, mapper in self.mappers.items():
-            dest_value = mapper(dest_attr, src)
-            values[dest_attr] = dest_value
-        return values
-
-    def __call__(self, src: Any) -> Any:
-        values = self._mapped_values(src)
-        return self.dest_type(**values)
-
-    @staticmethod
-    def get_attrs(dest_type: type) -> Sequence[str]:
-        dest_params = inspect.signature(dest_type.__init__).parameters
-        all_attrs = map(lambda e: e[0], dest_params.items())
-        dest_attrs = [e for e in all_attrs if e != 'self']
-        return dest_attrs
--- omapper/tests/test_mappers.py
+++ omapper/tests/test_mappers.py
@@ -1,18 +1,18 @@
+import copy
 from dataclasses import dataclass
 from datetime import datetime
 from typing import Sequence, Optional
 from unittest import TestCase
 
 from omapper import Mapper
-from omapper.mapper import Mappers
 
 
 class TestImplicitFullyAnnotated(TestCase):
     class Source:
-        def __init__(self):
-            self.first = 'Blub'
-            self.second = datetime(2021, 1, 2, 1, 2, 3, 4)
-            self.third = [1, 2, 7, 3]
+        def __init__(self, first=None, second=None, third=None):
+            self.first = 'Blub' if not first else first
+            self.second = datetime(2021, 1, 2, 1, 2, 3, 4) if not second else second
+            self.third = [1, 2, 7, 3] if not third else third
 
     @dataclass
     class TargetDataclass:
@@ -29,7 +29,7 @@ class TestImplicitFullyAnnotated(TestCase):
         expected = self.TargetObj(source.first, str(source.second.date()))
 
         mapper = Mapper(self.Source, self.TargetObj, mappers={
-            'second': Mappers.transform('second', lambda e: str(e.date()))
+            'second': lambda s: str(s.second.date())
         })
         actual = mapper(source)
 
@@ -41,7 +41,7 @@ class TestImplicitFullyAnnotated(TestCase):
         expected = self.TargetDataclass(str(source.second.date()), source.third)
 
         mapper = Mapper(self.Source, self.TargetDataclass, mappers={
-            'dt': Mappers.transform('second', lambda s: str(s.date()))
+            'dt': lambda s: str(s.second.date())
         })
         actual = mapper(source)
 
@@ -52,7 +52,7 @@ class TestImplicitFullyAnnotated(TestCase):
         self.assertRaises(
             ValueError, lambda: Mapper(
                 self.Source, self.TargetDataclass, mappers={
-                    'dt': Mappers.transform('second', lambda s: str(s.date()))}, explicit=True)
+                    'dt': lambda s: str(s.second.date())}, explicit=True)
         )
 
     def test_explicit_dataclass(self):
@@ -60,10 +60,110 @@ class TestImplicitFullyAnnotated(TestCase):
         expected = self.TargetDataclass(str(source.second.date()), source.third)
 
         mapper = Mapper(self.Source, self.TargetDataclass, mappers={
-            'dt': Mappers.transform('second', lambda s: str(s.date())),
-            'third': Mappers.identity('third'),
+            'dt': lambda s: str(s.second.date()),
+            'third': lambda s: s.third,
         })
         actual = mapper(source)
 
         self.assertEqual(expected.dt, actual.dt)
         self.assertEqual(expected.third, actual.third)
+
+    def test_check_identity_dest_attr(self):
+        source = self.Source()
+        expected = self.TargetDataclass(str(source.second.date()), source.third)
+
+        mapper = Mapper(self.Source, self.TargetDataclass, mappers={
+            'dt': lambda src: str(src.second.date()),
+            'third': lambda src: src.third,  # We test this method here
+        })
+        actual = mapper(source)
+
+        self.assertEqual(expected.dt, actual.dt)
+        self.assertEqual(expected.third, actual.third)
+
+    def test_identity_reference(self):
+        mapper = Mapper(self.Source, self.TargetDataclass, mappers={
+            'dt': lambda s: str(s.second.date())
+        })
+
+        source = self.Source()
+        dest = mapper(source)
+
+        original_source = list(source.third)  # Create copy
+        dest.third.append(111)
+
+        self.assertNotEqual(original_source, dest.third, msg="Just a self check that '111' was not already in source.")
+        self.assertEqual(source.third, dest.third, msg="Identity map should pass references and not values.")
+        self.assertEqual(source.third[-1], 111, "Value was added through 'dest'")
+
+    def test_copy_mapper(self):
+        mapper = Mapper(self.Source, self.Source, mappers={
+            'first': lambda src: copy.copy(src.first),
+            'second': lambda src: copy.copy(src.second),
+            'third': lambda src: copy.copy(src.third),
+        })
+
+        source = self.Source()
+        dest = mapper(source)
+        dest.third.append(111)
+
+        self.assertNotIn(111, source.third)
+
+    def test_implicit_default_mapper(self):
+        mapper = Mapper(self.Source, self.Source, default_mapper=lambda dest_attr: lambda src: getattr(src, dest_attr))
+
+        source = self.Source("AAA", "BBB", "CCC")
+        dest = mapper(source)
+        self.assertEqual(dest.first, "AAA")
+        self.assertEqual(dest.second, "BBB")
+        self.assertEqual(dest.third, "CCC")
+
+    class Deep:
+        def __init__(self, attr):
+            self.attr = attr
+
+    def test_deepcopy_mapper(self):
+        mapper = Mapper(self.Deep, self.Deep, mappers={
+            'attr': lambda s: copy.deepcopy(s.attr),
+        })
+
+        value = {'a': [1, 2, 3]}
+        source = self.Deep(value)
+        dest = mapper(source)
+        dest.attr['unexpected'] = [22, 33, ]
+        dest.attr['a'].append(111)
+
+        self.assertNotIn('unexpected', source.attr)
+        self.assertNotIn(111, source.attr['a'])
+
+    def test_missing_target_mapper_key(self):
+        def create_exception():
+            return Mapper(self.Deep, self.Deep, mappers={
+                'missing_target': lambda s: s.attr,
+            })
+        self.assertRaises(ValueError, create_exception)
+
+    def test_missing_source_in_mapper(self):
+        mapper = Mapper(self.Deep, self.Deep, mappers={
+            'attr': lambda s: s.missing_source,
+        })
+
+        source = self.Deep(123)
+        self.assertRaises(AttributeError, lambda: mapper(source))
+
+    def test_exception_in_ctor(self):
+        class Target:
+            def __init__(self, first):
+                raise AttributeError(f"Raise with value '{first}'.")
+
+        mapper = Mapper(self.Source, Target)
+
+        src = self.Source()
+        self.assertRaises(ValueError, lambda: mapper(src))
+
+    def test_target_has_empty_ctor(self):
+        class Target:
+            def __init__(self):
+                pass
+
+        self.assertRaises(ValueError, lambda: Mapper(self.Source, Target))
